5ce058378423ace40dd05567158e6a5a6d6de1a9
Gutiérrez Hermosillo Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2025-02-27 17:27:02 +0100

    wip: mem_const graph

--- tensorcraft/optim/redistribution/mem_const.py
+++ tensorcraft/optim/redistribution/mem_const.py
@@ -19,8 +19,9 @@ log = logging.getLogger("tensorcraft")
 @dataclasses.dataclass
 class Node:
     """Distribution graph nodes."""
+
     parent_node: Optional[Self]
-    dist: Dist
+    dist: MultiAxisDist
     children: dict[str, Self]
     cost: Cost
 
@@ -92,14 +93,118 @@ class MemoryConstrainedRedist(Redistributor):
         log.info(f"Helper dims: {helper_dims}")
 
         # 3) Move mesh dims around
-
         open_nodes: list[Node] = []
         close_nodes: list[Node] = []
+        end_nodes: list[Node] = []
+        
+        nodes_dict: dict[Dist, tuple[Node, float]] = {}
+                         
+        alpha = 0.1 # Latency weight
+        beta = 1    # Bandwidth weight
+        gamma = 1   # Memory weight
+
         starter_node = Node(None, start_dist, {}, Cost(0, 0, 0, 0))
         open_nodes.append(starter_node)
+        
+        nodes_dict[start_dist] = starter_node
+
+        base_memory = start_dist.maxNumElements()
+        memory_limit = max(base_memory, target_dist.maxNumElements())
 
         while len(open_nodes) > 0:
             current_node = open_nodes.pop(0)
+            close_nodes.append(current_node)
+            current_memory_usage = current_node.dist.maxNumElements(shape)
+            
+            # Free dims:
+            free_dims = []
+            for dim in range(len(mesh)):
+                is_free = True
+                for i in range(len(shape)):
+                    if (dim in start_dist._dims_mapping[i]):  # type: ignore
+                        is_free = False
+                        break
+
+                if is_free:
+                    free_dims.append(dim)
+            
+            # 1) Further split (use target block sizes)
+            for free_dim in free_dims:
+                if free_dim in helper_dims:
+                    for axis in range(len(shape)):
+                        if axis not in target_axes:
+                            operation = f"split_{axis}_{free_dim}_1"
+                            try:
+                                new_dist, _, _ = current_node.dist.split(shape, axis, free_dim, 1)
+                                log.debug(f"Landed in {new_dist}")
+                                
+                                memory_usage = new_dist.maxNumElements(memory_usage)
+                                
+                                op_cost = current_node.cost + Cost(0, 0, 0, memory_usage - current_memory_usage)
+                                
+                                result_node = Node(
+                                    current_node,
+                                    new_dist,
+                                    {},
+                                    op_cost
+                                )
+                                current_node.children[operation, result_node]
+                                
+                                if new_dist == target_dist:
+                                    end_nodes.append(result_node)
+                                else:
+                                    open_nodes.append(result_node)
+
+                            except:
+                                log.debug(f"Failed operation {operation} on dis {current_node.dist} with shape {shape}")
+                            
+            for target_axis in target_axes:
+                if current_node.dist._dims_mapping[target_axis] != target_dist._dims_mapping[target_axis]:
+                    c_axis_set = set(current_node.dist._dims_mapping[target_axis])
+                    t_axis_set = set(target_dist._dims_mapping[target_axis]) 
+                    extra_dims = c_axis_set - t_axis_set
+                    missing_dims = t_axis_set - c_axis_set
+                    
+
+                    if len(extra_dims) == 0 and len(missing_dims) == 0:
+                        # Needs permutation
+                        log.debug("Requires permutation")
+                    else:
+                        # Apply splits
+                        for m_dim in missing_dims:
+                            if m_dim in free_dim:
+                                target_block_size = target_dist._block_sizes[target_axis]
+                                operation = f"split_{target_axis}_{m_dim}_{target_block_size}"
+                                try:
+                                    new_dist, _, _ = current_node.dist.split(shape, target_axis, free_dim, target_block_size)
+                                    log.debug(f"Landed in {new_dist}")
+                                    
+                                    memory_usage = new_dist.maxNumElements(memory_usage)
+                                    
+                                    op_cost = current_node.cost + Cost(0, 0, 0, memory_usage - current_memory_usage)
+                                    
+                                    result_node = Node(
+                                        current_node,
+                                        new_dist,
+                                        {},
+                                        op_cost
+                                    )
+                                    current_node.children[operation, result_node]
+                                    
+                                    if new_dist == target_dist:
+                                        end_nodes.append(result_node)
+                                    else:
+                                        open_nodes.append(result_node)
+
+                                except:
+                                    log.debug(f"Failed operation {operation} on dis {current_node.dist} with shape {shape}")
+                        
+                        for e_dim in extra_dims:
+                            if 
+                                
+                    
+
+
 
         # 4) Adjust block sizes
 
